ef76f030b2813fcc2935626cceabfaf730b74c23
PrakharTTN <prakhar.nigam@tothenew.com>
2025-02-05 23:09:47 +0530

    Updated 3-ModulesAndPackages Files

--- 3-Modules-And-Packages/Q3.py
+++ 3-Modules-And-Packages/Q3.py
@@ -1,26 +1,37 @@
 import datetime
+from dateutil.relativedelta import relativedelta
 import itertools
 
 def datecount(start, step):
     
     step = step.lower()
     
+    # For Monthly, Quarterly & Yearly, using relativedelta to calculate months instead of days.
     step_dict = {
         'alternative': 2,
         'daily': 1,
         'weekly': 7,
-        'monthly': 30,  # Approximation
-        'quarterly': 91,  # Approximate 3 months
-        'yearly': 365
+        'monthly': 1,  # 1 month = month
+        'quarterly': 3,  # 3 months = quarter
+        'yearly': 12 # 12 months = year
     }
     
     if step not in step_dict:
         raise ValueError("Invalid step. Choose from 'alternative', 'daily', 'weekly', 'monthly', 'quarterly', or 'yearly'.")
+    
+    Days=['daily','weekly','alternative']
 
-    step_days = step_dict[step]
-    while True:
-        yield start
-        start += datetime.timedelta(days=step_days)
+    if step in Days:
+        step_days = step_dict[step]
+        while True:
+            yield start
+            start += datetime.timedelta(days=step_days)
+    else:
+        step_days=step_dict[step]
+        while True:
+            yield start
+            start+=relativedelta(months=step_days)
+
 
 step=str(input("Enter the step: "))
 dc = datecount(start=datetime.date.today(), step=step)
