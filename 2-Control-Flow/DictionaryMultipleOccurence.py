9b7bb5ccee9df1d8146985a9afaded8934ee9621
PrakharTTN <prakhar.nigam@tothenew.com>
2025-02-10 09:51:48 +0530

    Modified DictMultiOccurence

--- 2-Control-Flow/DictionaryMultipleOccurence.py
+++ 2-Control-Flow/DictionaryMultipleOccurence.py
@@ -1,7 +1,7 @@
 """Q4. Programing: From a multi-words and multi-line string, display list of words and word's length 
 with occurrence more than 1 in sorted order """
 
-string = """Python Multiline String Using Triple-Quotes 
+astring = """Python Multiline String Using Triple-Quotes 
 
 Using the triple quotes style is one of the easiest and most common ways to split a large string into a multiline Python string. Triple quotes (''' or \""") can be used to create a multiline string. It allows you to format text over many lines and include line breaks. Put two triple quotes around the multiline Python string, one at the start and one at the end, to define it."""
 
