a7b727a0e75c19523767fb66481c2c7788af4120
PrakharTTN <prakhar.nigam@tothenew.com>
2025-02-10 10:03:51 +0530

    4-File-Handling Updated Filenames & Added Questions Comments

--- 4-File-Handling/numberHandling/numberFileHandling.py
+++ 4-File-Handling/numberHandling/numberFileHandling.py
@@ -1,3 +1,12 @@
+"""Q1. You have a number.txt, with each line a real number. Write a code to split this file into 3 files as follows:
+
+even.txt  -- contain all even numbers
+odd.txt -- all odd number
+float.txt -- all floating point number
+
+Use with() clause for file handling
+"""
+
 with open("number.txt","r") as numbers, open("even.txt",'w+') as evenNumbers, open("odd.txt",'w+') as oddNumbers, open("float.txt",'w+') as floatNumbers:
     for i in numbers.readlines():
             if ('.' not in i) and int(i)%2==0:
