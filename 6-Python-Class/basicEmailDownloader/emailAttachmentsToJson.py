ecf0976a8f8c34739fc2013f6966c07512df47d3
PrakharTTN <prakhar.nigam@tothenew.com>
2025-02-14 15:47:36 +0530

    Updated code with PEP-8 style

--- 6-Python-Class/basicEmailDownloader/emailAttachmentsToJson.py
+++ 6-Python-Class/basicEmailDownloader/emailAttachmentsToJson.py
@@ -21,80 +21,80 @@ import pickle
 import json
 from email.header import decode_header
 
-class emailEnvironment:
+class EmailEnvironment:
 
     def __init__(self):
-        self.user=""
-        self.passw=""
-        self.session= None
-        self.emails=[]
+        self.user = ""
+        self.passw= ""
+        self.session = None
+        self.emails = []
     
-    #This function dumps the details from emails[] into JSON files.
+    '''This function dumps the details from emails[] into JSON files.'''
     def dump(self):
-        with open("Email.json","w") as file, open("EmailPickle.json",'wb') as file2:
+        with open("Email.json", "w") as file, open("EmailPickle.json", "wb") as file2:
             for i in self.emails:
-                json.dump(i,file,indent=2)
-                pickle.dump(i,file2)
+                json.dump(i, file, indent=2)
+                pickle.dump(i, file2)
     
-    #This function loads the config.yaml file and safe loads and parses the username and password
-    def loading_creds(self,filepath):
+    '''This function loads the config.yaml file and safe loads and parses the username and password'''
+    def loading_creds(self, filepath):
         try:
-            with open(filepath,'r') as creds:
-                credentials=yaml.safe_load(creds)
-                self.user=credentials['user']
-                self.passw=credentials['password']
+            with open(filepath, 'r') as creds:
+                credentials = yaml.safe_load(creds)
+                self.user = credentials['user']
+                self.passw = credentials['password']
                 print("Successfully read the config file with credentials.")
                 self.connection()
         except Exception as e:
             print("Failed to load credentials.")
     
-    #This function creates a session with my gmail account
+    '''This function creates a session with my gmail account'''
     def connection(self):
         try:
-            self.session =imaplib.IMAP4_SSL('imap.gmail.com')
+            self.session = imaplib.IMAP4_SSL('imap.gmail.com')
             self.session.login(self.user,self.passw)
             print("Access has been granted. Fetching mail details.")
             self.fetch_emails()
         except Exception as e:
             print("Failed to connect, check email/password. ")
 
-    #This function checks for attachments in the mail and 
-    #Returns a list with names of emails and total number of attachments
+    '''This function checks for attachments in the mail and 
+    Returns a list with names of emails and total number of attachments'''
     def get_attachments(self,msg):
-        attachments=[]
+        attachments = []
         for part in msg.walk():
             content_disposition = part.get('Content-Disposition')
             if content_disposition and 'attachment' in content_disposition.lower():
                 attachments.append(part.get_filename())
-        if len(attachments)==0:
-            return None,0        
-        return attachments,len(attachments)
+        if len(attachments) == 0:
+            return None, 0        
+        return attachments, len(attachments)
     
-    #This function checks the body from the mail and returns total words and total lines
-    def get_body(self,msg):
-        body_length=0
-        body_lines=0
+    '''This function checks the body from the mail and returns total words and total lines'''
+    def get_body(self, msg):
+        body_length = 0
+        body_lines = 0
         for part in msg.walk():
             # Get Body by checking if it is plain text
             if part.get_content_type() == "text/plain":
                 # Decode the body
                 body = part.get_payload(decode=True).decode()
-                body_lines=body.count('\n')
-                body_length=len(body.split()) 
+                body_lines = body.count('\n')
+                body_length = len(body.split()) 
         return body_length,body_lines
 
-    # Fetch all emails from inbox
+    '''Fetch all emails from inbox'''
     def fetch_emails(self):
-        maxemails=5 #Specified total emails I want to download
+        max_emails = 5 #Specified total emails I want to download
         self.session.select("inbox")
-        status, messages = self.session.search(None, "ALL") 
-        email_ids = messages[0].split()[-maxemails:]
+        messages = self.session.search(None, "ALL") 
+        email_ids = messages[0].split()[-max_emails:]
 
         for email_id in email_ids:
             # Fetch each email
             status, data = self.session.fetch(email_id, "(RFC822)")
             for response in data:
-                if type(response) is tuple:
+                if isinstance(response) is tuple:
 
                     #Response[0] is the header file, while [1] is the main body
                     msg = email.message_from_bytes(response[1]) 
@@ -103,7 +103,7 @@ class emailEnvironment:
                     subject = decode_header(msg["subject"])[0][0]  
                     date = msg["date"]
                     sender = msg["from"]
-                    body_length,body_lines = self.get_body(msg)
+                    body_length, body_lines = self.get_body(msg)
                     attachment, total_attachments = self.get_attachments(msg)
                     self.emails.append({
                         "subject": subject,
@@ -123,6 +123,6 @@ class emailEnvironment:
 
 #Specified path of my config
 path="/home/prakhar/Desktop/config.yaml"
-a=emailEnvironment()
+a=EmailEnvironment()
 a.loading_creds(path)
 
