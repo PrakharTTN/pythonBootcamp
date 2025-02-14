cb5806d43c7bc85df70f30929d502487a8500198
PrakharTTN <prakhar.nigam@tothenew.com>
2025-02-14 12:25:10 +0530

    Updated requestHandling.py

--- 9-Doubts/requestHandling.py
+++ 9-Doubts/requestHandling.py
@@ -1,27 +1,53 @@
 import requests
 from requests.exceptions import *
 
-class requestHandling:
+
+class RequestHandling:
     def __init__(self, url):
         self.url = url
-    def get(self,payload=None):
+
+    def get(self,header,timeout=10,payload=None):
+        '''Implements the get'''
         try:
-            response = requests.get(self.url,params=payload,timeout=10)
+            if payload:
+                response = requests.get(self.url, params = payload, timeout=timeout)
+            else:
+                response = requests.get(url=self.url,headers=header,timeout=timeout)
             response.raise_for_status() 
             return response
-        except requests.exceptions.HTTPError as error:
-            print("HTTP error found:", error)
-        except requests.exceptions.ConnectionError as error:
-            print("Connection error found: ",error)
-        except requests.exceptions.Timeout as error:
-            print("Connection Timedout: ",error)
-        except requests.exceptions.RequestException as error:
-            print("Other Error: ", error)
-        except requests.exceptions.TooManyRedirects as error:
-            print("Too many redirects from server: ",error)
+            
+            '''Handling all the exceptions here'''
+        except requests.exceptions.InvalidHeader as error:
+            print("Invalid Header Provided.")
 
-singlerequest = requestHandling("http://www.google.com")
-final = singlerequest.get()
+        except requests.exceptions.HTTPError as error:
+            print("HTTP error found. \n", self.HTTP_error(response.status_code))
+        
+        except requests.exceptions.ConnectionError as error:
+            print("Connection error found: ")
+        
+        except requests.exceptions.Timeout as error:
+            print("Connection Timedout: ")
+        
+        except requests.exceptions.RequestException as error:
+            print("Other Error: ")
+
+        except requests.exceptions.TooManyRedirects as error:
+            print("Too many redirects from server: ")
+
+    def HTTP_error(self, error_code):
+        '''This block is to return respective HTTP error.'''
+        error_dict = {
+            404: "Error 404: Resource not found",
+            500: "Error 500: Internal server error",
+            503: "Error 503: Service unavailable",
+            401: "Error 401: Unauthorized",
+            403: "Error 403: Forbidden",
+        }
+        return error_dict.get(error_code)
+
+singlerequest = RequestHandling("http://www.google.com")
+final = singlerequest.get(header={},timeout=10)
 
 if final:
     print("Success:", final)
