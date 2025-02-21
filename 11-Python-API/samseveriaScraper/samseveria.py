db5173ed5d9cccdee4087fbc53ec861d89ce385e
PrakharTTN <prakhar.nigam@tothenew.com>
2025-02-21 09:46:58 +0530

    Scraper to scrape outside page

--- 11-Python-API/samseveriaScraper/samseveria.py
+++ 11-Python-API/samseveriaScraper/samseveria.py
@@ -4,54 +4,68 @@ from urllib.parse import urljoin
 import pandas as pd
 
 
-# For page number (For now provided 1, can input from user)
-number = 1
-url = f"https://fermosaplants.com/collections/sansevieria?page={number}"
+def xlsx_dump(list_final):
+    with pd.ExcelWriter("ScrapedData.xlsx") as writer:
+        for i in range(1, 8):
+            dataframe = pd.DataFrame(list_final[i - 1])
+            dataframe.to_excel(writer, sheet_name=f"page {i}")
 
-# Send request to the URL
-webpage = requests.get(url=url)
-sp = BeautifulSoup(webpage.content, "html.parser")
 
-listfinal = []
-type_plant = ["combo", "clump", "leaf", "plant"]
+def main():
+    # For page number (For now provided 1, can input from user)
+    number = 7
+    final_list = []
+    for i in range(1, 8):
+        url = f"https://fermosaplants.com/collections/sansevieria?page={i}"
 
-# Loop through all product items
-for product in sp.find_all("div", class_="product-item-v5"):
-    # Get the URL of the product
-    urls = product.find("a")["href"]
+        # Send request to the URL
+        webpage = requests.get(url=url)
+        sp = BeautifulSoup(webpage.content, "html.parser")
 
-    # Get the product name
-    names = product.find("h4", class_="title-product")
+        sub_list = []
+        type_plant = ["combo", "clump", "leaf", "plant"]
 
-    list_of_names = names.text.lower().replace("(", "").replace(")", "").split()
-    # Get the type
-    types = "Plant"
-    for i in list_of_names:
-        if i in type_plant:
-            types = i
-            break
+        # Loop through all product items
+        for product in sp.find_all("div", class_="product-item-v5"):
+            # Get the URL of the product
+            urls = product.find("a")["href"]
 
-    # Get units if combo
-    unit = 1
-    for i in list_of_names:
-        if i.isdigit():
-            unit = i
-            break
+            # Get the product name
+            names = product.find("h4", class_="title-product")
 
-    # Get the price
-    price = product.find("span", class_="price")
+            list_of_names = names.text.lower().replace("(", "").replace(")", "").split()
+            # Get the type
+            types = "Plant"
+            for i in list_of_names:
+                if i in type_plant:
+                    types = i
+                    break
 
-    listfinal.append(
-        {
-            "Name": names.text.strip(),
-            "Type": types,
-            "Total Units": unit,
-            "Price": price.text,
-            "URL": urljoin(url, urls),
-        }
-    )
+            # Get units if combo
+            unit = 1
+            for i in list_of_names:
+                if i.isdigit():
+                    unit = i
+                    break
 
-dataframe = pd.DataFrame(listfinal)
-dataframe.to_excel(f"scraped_data_page{number}.xlsx")
+            # Get the price
+            price = product.find("span", class_="price")
 
-print("Successfully Scraped Data and dumped into xlsx file.")
+            sub_list.append(
+                {
+                    "Name": names.text.strip(),
+                    "Type": types,
+                    "Total Units": unit,
+                    "Price": price.text,
+                    "URL": urljoin(url, urls),
+                }
+            )
+
+        final_list.append(sub_list)
+    print(final_list)
+    xlsx_dump(final_list)
+    print("Successfully Scraped Data and dumped into xlsx file.")
+
+
+if __name__ == "__main__":
+    main()
