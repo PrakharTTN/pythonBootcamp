24041a28b97b32d4511fe7d171d033c0a15f9d96
PrakharTTN <prakhar.nigam@tothenew.com>
2025-02-25 10:44:48 +0530

    Updated Week-2/08-Prebuild-Design directory

---
+++ Week_2_Class/07_Object_Oriented/singletonClass.py
@@ -0,0 +1,18 @@
+'''Q3. Implement a singleton class `Database` that ensures 
+only one instance of the class can be created.'''
+
+class Database:
+    has_obj=None
+    def __new__(cls):
+        if cls.has_obj == None:
+            cls.has_obj = super().__new__(cls)
+            print("The first instance has been creeated")
+        else:
+            print("The instance already exists.")
+        return cls
+    
+obj1 = Database() 
+obj2 = Database()
+print("Unique ID of obj1: ", id(obj1))
+print("Unique ID of obj2: ", id(obj2))
+print("Checking if obj1=obj2: ", obj1==obj2)
---
+++ Week_2_Class/07_Object_Oriented/singletonExplained.py
@@ -0,0 +1,33 @@
+import gc
+
+class Database:
+    has_obj = None
+    
+    def __new__(cls):
+        if cls.has_obj is None:
+            cls.has_obj = super().__new__(cls)
+        return cls.has_obj
+    
+    @classmethod
+    def refdel(cls):
+        cls.has_obj = None
+
+# Create instances of Database
+db1 = Database()
+db2 = Database()
+
+print(f"id(db1): {id(db1)}")  # id(db1) and id(db2) should be the same
+print(f"id(db2): {id(db2)}")  # because they refer to the same object
+
+# Delete the instances
+del db1
+del db2
+
+# Explicitly clear the singleton reference
+Database.refdel()  # Manually clear the singleton instance reference
+
+# Call garbage collector to cleanup
+gc.collect()
+# Create a new instance, which will be a fresh instance
+db3 = Database()
+print(f"id(db3): {id(db3)}")  # id(db3) should be a new, different instance
