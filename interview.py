71c5b1ef670678c4432b25b00fc6cde472890d37
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-03 16:38:33 +0530

    Interview questions

---
+++ interview.py
@@ -0,0 +1,17 @@
+"""WAP to remove duplicate items from the list and preserve the order of the list
+['a','b','c','a','d','b','e']
+['a','b','c','d','e']"""
+
+"""Class method, instance method,static method"""
+
+
+# def func(a, b, *args, **kwargs):
+#     pass
+
+
+# func(1, 2, 3, 4, name="")
+
+# a = 1
+# b = 2
+# (3, 4)
+# name: ""
