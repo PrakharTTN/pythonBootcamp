5f8843b3ed89ce16de55b57c0d502a74d3264823
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-04 17:24:52 +0530

    deleted pollapp

--- Week_4_Django/14-Django/poll/pollapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Question(models.Model):
-    id = models.AutoField(primary_key=True)
-    description = models.CharField(max_length=100)
-    answer = models.CharField(choices=[("yes", "yes"), ("no", "no")], max_length=3)
--- Week_4_Django/14-Django/poll/pollapp/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-
-class SimpleViewTest(TestCase):
-    def test_simple_view(self):
-        response = self.client.get(reverse("simple_view"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Hello, this is a simple view!")
