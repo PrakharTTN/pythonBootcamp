f953b8383bd126da0751953ead92d94845797104
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-05 17:13:17 +0530

    Added: Proper Copying

--- Week_2_Class/09_Doubts/checkCopy.py
+++ Week_2_Class/09_Doubts/checkCopy.py
@@ -1,6 +1,10 @@
 import copy
 from copy import deepcopy
-list1=[[1],[2,3]]
-list2=deepcopy(list1)
-list1[1][1]=5
+
+list1 = [[1], [2, 3]]
+list2 = deepcopy(list1)
+list3 = list1.copy()
+list1[1][1] = 5
+print(list1)
 print(list2)
+print(list3)
