48e15910c3db2aa24ee39d283936a95aba61831f
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-08 21:11:53 +0530

    Added: Code for logging

--- Week_4_Django/14-Django/poll/greeting/views.py
+++ Week_4_Django/14-Django/poll/greeting/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from django.http import HttpResponseForbidden
 from django.contrib.auth.mixins import LoginRequiredMixin
+import logging
 from django.shortcuts import redirect
 from .models import Poll, Vote
 from .forms import VoteForm
@@ -24,6 +25,7 @@ class PollDetailView(DetailView):
     def post(self, request):
         return redirect("greeting:vote_from")
 
+logger = logging.getLogger('user_actions')
 
 class VoteFormView(FormView):
     template_name = "vote_form.html"
@@ -37,5 +39,30 @@ class VoteFormView(FormView):
             return HttpResponseForbidden("You have already voted for poll.")
 
         # Save the vote
+
         Vote.objects.create(poll=poll, user=username)
+        logger.info(f'User  {username} voted for: {poll}')
         return super().form_valid(form)
+
+
+# def active_polls_view(request):
+
+#     active_polls = Poll.objects.active()  # Use the custom manager method
+
+#     return render(request, 'active_polls.html', {'polls': active_polls})
+
+
+# def recent_polls_view(request):
+
+#     recent_polls = Poll.objects.created_within_last_week()  # Use the custom manager method
+
+#     return render(request, 'recent_polls.html', {'polls': recent_polls})
+
+
+# def votes_for_poll_view(request, poll_id):
+
+#     poll = Poll.objects.get(id=poll_id)
+
+#     votes = Vote.objects.for_poll(poll)  # Use the custom manager method
+
+#     return render(request, 'votes_for_poll.html', {'poll': poll, 'votes': votes})
