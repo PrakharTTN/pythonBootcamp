694a6718a3580ca2c99d7b91dc56eb17bff583d2
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-08 21:11:40 +0530

    Added: Poll Manager for models

--- Week_4_Django/14-Django/poll/greeting/models.py
+++ Week_4_Django/14-Django/poll/greeting/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from datetime import timedelta,timezone
 from django.contrib.auth.models import User
 
+class PollManager(models.Manager):
+    def active(self):
+        return self.filter(is_active=True)
 
+    def created_within_last_week(self):
+        one_week_ago = timezone.now() - timedelta(days=7)
+        return self.filter(created_at__gte=one_week_ago)
+    
+class VoteManager(models.Manager):
+    def for_poll(self, poll):
+        return self.filter(poll=poll)
+
+    
 class Poll(models.Model):
     question_text = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
