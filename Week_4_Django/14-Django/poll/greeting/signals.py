b0719d02610039531cef560ac4f267e4341f569d
PN <prakhar.nigam@tothenew.com>
2025-03-09 22:06:40 +0530

    Added: Signal upon poll creation

---
+++ Week_4_Django/14-Django/poll/greeting/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Poll
+import logging
+
+logger = logging.getLogger("poll_signals")
+
+@receiver(post_save, sender=Poll)
+def log_poll_creation(sender, instance, created, **kwargs):
+    if created:
+        logger.info(f"New poll created: {instance.question_text}")
