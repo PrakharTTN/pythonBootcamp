5a73446e88d325ffab1e4b5eb0f4c9e14c313703
PN <prakhar.nigam@tothenew.com>
2025-03-09 22:05:32 +0530

    Added: Signal added

--- Week_4_Django/14-Django/poll/greeting/apps.py
+++ Week_4_Django/14-Django/poll/greeting/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class GreetingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'greeting'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "greeting"
+
+    def ready(self):
+        import greeting.signals
