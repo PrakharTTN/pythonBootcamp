04df285ede904d0d9e8ae16f5854f9fe9eea6772
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-10 15:41:20 +0530

    Updated: Added Excel form view

--- Week_4_Django/14-Django/poll/greeting/forms.py
+++ Week_4_Django/14-Django/poll/greeting/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Vote
 
 
@@ -6,3 +7,15 @@ class VoteForm(forms.ModelForm):
     class Meta:
         model = Vote
         fields = ["poll", "user"]
+
+
+class UploadExcelForm(forms.Form):
+    file = forms.FileField()
+
+    def clean_file(self):
+        file = self.cleaned_data.get("file")
+        if file.size > 10 * 1024 * 1024:  # size to be 10MB
+            raise ValidationError("File size must be under 10 MB.")
+
+        if not file.name.endswith(".xlsx"):  # should be xlsx file
+            raise ValidationError("Only .xlsx files are allowed.")
