d9ad63c360ca5ba2b2c667e2e29d618413e92cf8
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-10 15:42:40 +0530

    Updated: UploadExcel URL

--- Week_4_Django/14-Django/poll/greeting/urls.py
+++ Week_4_Django/14-Django/poll/greeting/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PollListView, PollDetailView, VoteFormView
+from .views import PollListView, PollDetailView, VoteFormView, UploadFileView
 
 app_name = "polls"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", PollListView.as_view(), name="poll_list"),
     path("poll/<int:pk>/", PollDetailView.as_view(), name="poll_detail"),
     path("poll/<int:poll_id>/vote/", VoteFormView.as_view(), name="vote_form"),
+    path("upload_excel/", UploadFileView.as_view(), name="excel_upload"),
 ]
