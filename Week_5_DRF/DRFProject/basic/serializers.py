70f3b0b83295adaca00bcbb2b122332406f4f271
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-12 21:04:31 +0530

    Created Item Serializer

---
+++ Week_5_DRF/DRFProject/basic/serializers.py
@@ -0,0 +1,23 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Item
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    """This class is to serialize the ITEM DB data"""
+
+    def validate(self, data):
+        """This method validates the data based on the ITEM_NAME if it exists"""
+
+        if Item.objects.filter(name=data["name"]).exists():
+            raise serializers.ValidationError("Item already exists")
+        return data
+
+    class Meta:
+        model = Item
+        fields = ["name", "description", "price", "quantity"]
+
+
+class UpdateItemSerializer(ItemSerializer):
+    def validate(self, data):
+        return data
