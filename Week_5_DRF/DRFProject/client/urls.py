41ecca6d97a080d7c94691997ef2a2b3bf6a398a
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-12 21:07:19 +0530

    Creation: Added router for client order

---
+++ Week_5_DRF/DRFProject/client/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserOrderModelViewSet
+
+router = DefaultRouter()
+router.register(r"orders", UserOrderModelViewSet)
+
+urlpatterns = [
+    path("client/", include(router.urls)),
+]
