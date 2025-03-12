4f06300d0a3d2a546c6b95301e2124db2c198da9
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-12 21:04:16 +0530

    Created: Model for Items

---
+++ Week_5_DRF/DRFProject/basic/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .validators import validate_name, validate_number, validate_quantity
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, validators=[validate_name])
+    description = models.TextField()
+    price = models.IntegerField(validators=[validate_number])
+    quantity = models.IntegerField(
+        default=1, validators=[validate_number, validate_quantity]
+    )
