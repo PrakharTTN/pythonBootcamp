34c11e1b9f4341f0667d171b2af3bb196fc508cc
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-13 14:51:07 +0530

    Updated: Serializers updated

--- Week_5_DRF/DRFProject/client/serializers.py
+++ Week_5_DRF/DRFProject/client/serializers.py
@@ -1,49 +1,68 @@
 from rest_framework import serializers
-from .models import UserOrderModel, Item, OrderItem
+from .models import UserOrderModel, OrderItem
+from basic.models import Item
 from django.contrib.auth.models import User
 
-# Serializer for Item model
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = '__all__'
 
-# Serializer for OrderItem model (Intermediate Model for Many-to-Many Relationship)
+# class ItemSerializer(serializers.ModelSerializer):
+#     """Serializer for Item model"""
+
+#     class Meta:
+#         model = Item
+#         fields = "__all__"
+
+
 class OrderItemSerializer(serializers.ModelSerializer):
-    item = ItemSerializer()  # Nesting ItemSerializer to include item details in the response
+    """Serializer for OrderItem model (Mid- Model for MtoM Relationship)"""
+
+    item = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all())
 
     class Meta:
         model = OrderItem
-        fields = ['item', 'quantity']
+        fields = ["item", "quantity"]
 
-# Serializer for UserOrderModel
+class OrderItemSerializer(serializers.ModelSerializer):
+    """Serializer for OrderItem model (Mid- Model for MtoM Relationship)"""
+    
+    item = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all())
+
+    class Meta:
+        model = OrderItem
+        fields = ["item", "quantity"]
+
+
 class UserOrderModelSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  # Linking to User model
-    items = OrderItemSerializer(many=True)  # Nested OrderItemSerializer to handle the items in the order
-    total_price = serializers.IntegerField(read_only=True)  # We can calculate total_price dynamically
+    """Serializer for UserOrderModel"""
+
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    items = OrderItemSerializer(many=True)
+    total_price = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = UserOrderModel
-        fields = ['id', 'user', 'items', 'order_date', 'total_price']
+        fields = ["id", "user", "items", "order_date", "total_price"]
 
     def create(self, validated_data):
-        # Handle creation of UserOrderModel and OrderItems
-        items_data = validated_data.pop('items')
+        """Handle creation of UserOrderModel and OrderItems"""
+        items_data = validated_data.pop("items")
         order = UserOrderModel.objects.create(**validated_data)
 
-        # Create OrderItem instances
+        # Iterate over items_data, where "item" is now the full Item object, not just the ID
         for item_data in items_data:
-            item = item_data['item']
-            quantity = item_data['quantity']
+            item = item_data["item"]  # This is an actual Item object
+            quantity = item_data["quantity"]
+
+            # Ensure that we're passing the item ID to the OrderItem
             OrderItem.objects.create(order=order, item=item, quantity=quantity)
 
-        order.calculate_total()  # Calculate total price for the order
+        order.calculate_total()
         return order
 
     def update(self, instance, validated_data):
-        # Handle updating of the UserOrderModel and OrderItems
-        items_data = validated_data.pop('items', None)
-        instance.user = validated_data.get('user', instance.user)
+        """Handle updating of the UserOrderModel and OrderItems"""
+
+        items_data = validated_data.pop("items", None)
+        instance.user = validated_data.get("user", instance.user)
 
         instance.save()
 
@@ -51,8 +70,8 @@ class UserOrderModelSerializer(serializers.ModelSerializer):
             # Remove existing OrderItems and add new ones
             instance.items.clear()
             for item_data in items_data:
-                item = item_data['item']
-                quantity = item_data['quantity']
+                item = item_data["item"]
+                quantity = item_data["quantity"]
                 OrderItem.objects.create(order=instance, item=item, quantity=quantity)
 
         instance.calculate_total()  # Recalculate total price after update
