339d9ac94250818cbc4cff586611b0ed2ec1e7f8
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-13 14:49:38 +0530

    Updated: Added ID field

--- Week_5_DRF/DRFProject/basic/models.py
+++ Week_5_DRF/DRFProject/basic/models.py
@@ -3,6 +3,7 @@ from .validators import validate_name, validate_number, validate_quantity
 
 
 class Item(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, validators=[validate_name])
     description = models.TextField()
     price = models.IntegerField(validators=[validate_number])
