e75b785c0c7539a420341d7a0c756bb97edb6d8a
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-13 14:50:45 +0530

    Added: MtoM model for OrderItems

--- Week_5_DRF/DRFProject/client/models.py
+++ Week_5_DRF/DRFProject/client/models.py
@@ -7,7 +7,7 @@ class UserOrderModel(models.Model):
     """This model stores the all the orders of all the user"""
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # items = models.ManyToManyField(Item, through="OrderItem")
+    items = models.ManyToManyField(Item, through="OrderItem")
     order_date = models.DateTimeField(auto_now_add=True)
     total_price = models.IntegerField(default=0)
 
@@ -19,17 +19,18 @@ class UserOrderModel(models.Model):
         and considering the quantity ordered"""
 
         self.total_price = sum(
-            item.price * order_item.quantity for item, order_item in self.items.all()
+            order_item.item.price * order_item.quantity
+            for order_item in self.orderitem_set.all()
         )
         self.save()
 
 
-# class OrderItem(models.Model):
-#     """Intermediate model for the ManyToMany relationship between UserOrderModel and Item"""
+class OrderItem(models.Model):
+    """Intermediate model for the ManyToMany relationship between UserOrderModel and Item"""
 
-#     order = models.ForeignKey(UserOrderModel, on_delete=models.CASCADE)
-#     # item = models.ForeignKey(Item, on_delete=models.CASCADE)
-#     quantity = models.PositiveIntegerField(default=1)
+    order = models.ForeignKey(UserOrderModel, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
 
-#     def __str__(self):
-#         return f"{self.quantity} x {self.item.name} in Order {self.order.id}"
+    def __str__(self):
+        return f"{self.quantity} x {self.item.name} in Order {self.order.id}"
