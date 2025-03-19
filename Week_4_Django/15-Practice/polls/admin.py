336970b86936eea3b2548ebccb83626aaf148109
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-19 17:02:22 +0530

    Updated: QuestionModel via fieldsets

--- Week_4_Django/15-Practice/polls/admin.py
+++ Week_4_Django/15-Practice/polls/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import QuestionModel, ChoiceModel
 
+
 # Register your models here.
-admin.site.register(QuestionModel)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+
+admin.site.register(QuestionModel, QuestionAdmin)
+
 admin.site.register(ChoiceModel)
