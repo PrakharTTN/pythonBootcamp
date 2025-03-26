bab5df8dafab4702c231db4037330b33749024ee
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-26 15:37:30 +0530

    Updated: Updated URL for PUT,PATHC AND DELETE

--- Week_5_DRF/DRFProject/basic/urls.py
+++ Week_5_DRF/DRFProject/basic/urls.py
@@ -4,6 +4,6 @@ from .views import StaffItemView
 urlpatterns = [
     path("items/", StaffItemView.as_view(), name="staff_item"),  # For POST AND GET
     path(
-        "items/<str:name>", StaffItemView.as_view(), name="staff_update"
+        "items/<int:id>", StaffItemView.as_view(), name="staff_update"
     ),  # FOR PUT,PATCH,DELETE
 ]
