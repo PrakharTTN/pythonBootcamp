f1faf846302fee7016dc11d6b2b8ec7e099b2366
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-28 14:26:57 +0530

    Updated: Changed Choice FIeld

--- Week_4_Django/15-Practice/polls/models.py
+++ Week_4_Django/15-Practice/polls/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 from datetime import date
-
-CHOICES = [
-    ("yes", "yes"),
-    ("no", "no"),
-]
+from django.utils import timezone
 
 
 class QuestionModel(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(default=date.today)
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.question_text
 
 
 class ChoiceModel(models.Model):
     question = models.ForeignKey(
         QuestionModel, on_delete=models.CASCADE, related_name="choice"
     )
-    choice = models.CharField(choices=CHOICES, max_length=3)
+    choice = models.CharField(max_length=20)
     votes = models.BigIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.question.question_text} - {self.choice.title()}"
