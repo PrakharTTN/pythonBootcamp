bb5e8e433019f9033625b7624cbda7ff3eada2e0
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-28 14:21:47 +0530

    Updated: Static Files Directory

--- Week_4_Django/14-Django/currycrown/currycrown/settings.py
+++ Week_4_Django/14-Django/currycrown/currycrown/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -139,4 +141,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Media URLs
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-LOGIN_URL ="/login/"
+LOGIN_URL = "/login/"
