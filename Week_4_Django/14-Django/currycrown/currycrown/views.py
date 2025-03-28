16f6797531a35a794d2ad575dd1e37f162efa77e
PrakharTTN <prakhar.nigam@tothenew.com>
2025-03-28 14:22:17 +0530

    Updated: Added Landing Page (main)

--- Week_4_Django/14-Django/currycrown/currycrown/views.py
+++ Week_4_Django/14-Django/currycrown/currycrown/views.py
@@ -3,6 +3,16 @@ from management.models import Menu
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserCreationForm
+from django.contrib.auth import logout
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("landing_page")
 
 
 def user_login(request):
@@ -12,10 +22,11 @@ def user_login(request):
             user = form.get_user()
             login(request, user)
             if user.is_staff:
-                return redirect("management:landing_page")
+                return redirect("landing_page")
             else:
                 return redirect("customer:view_menu")
-
+    if request.user.is_authenticated:
+        return redirect("landing_page")
     form = AuthenticationForm()
     return render(request, "registration/login.html", {"form": form})
 
